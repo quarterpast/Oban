e3fadb46f90dc65258c492b6681f57496967163f
Matt Brennan <mattyb1000@gmail.com>
2015-08-01 12:33:28 +0100

    feat: start again with response stream
    BREAKING CHANGE: this is now a totally different library

---
+++ src/index.js
@@ -0,0 +1,50 @@
+var MetaStream = require('@quarterto/meta-stream');
+
+var Response = MetaStream.use({
+	getInitialMeta() {
+		return {
+			status: 200,
+			headers: {}
+		}
+	},
+
+	pipe(req) {
+		req.writeHead(this.meta().status, this.meta().headers);
+		return σ().pipe.call(this, req);
+	},
+
+	status(status) {
+		this.meta({status});
+		return this;
+	},
+
+	header(k, v) {
+		return this.headers({[k]: v});
+	},
+
+	headers(headers) {
+		this.meta({headers});
+		return this;
+	}
+}, {
+	body(body) {
+		return this(
+			_.isStream(body) || body.pipe? body
+			: /* otherwise */              [].concat(body)
+		);
+	},
+
+	ok(body) {
+		return this.body(body).status(200);
+	},
+
+	notFound(body) {
+		return this.body(body).status(404);
+	},
+
+	redirect(location, status = 302) {
+		return this.body([]).status(status).header('location', location);
+	}
+}
+
+module.exports = Response;
