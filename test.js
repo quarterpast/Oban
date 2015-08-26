e9b3e9bd467b257603d4a03e603f4d8c459b2c05
Matt Brennan <mattyb1000@gmail.com>
2015-08-26 20:38:07 +0100

    chore(tests): test static helpers

--- test.js
+++ test.js
@@ -34,5 +34,113 @@ exports['Response'] = {
 			};
 			r.pipe(s);
 		}
+	},
+
+	'body': {
+		'should write string body' (done) {
+			Response.body('hello').toArray(body => {
+				expect(body).to.deep.equal(['hello']);
+				done();
+			});
+		},
+
+		'should pipe stream body' (done) {
+			Response.body(σ(['hello'])).toArray(body => {
+				expect(body).to.deep.equal(['hello']);
+				done();
+			});
+		}
+	},
+
+	'ok': {
+		'should set body' (done) {
+			var R = Response.use({}, {
+				body(body) {
+					expect(body).to.equal('hello');
+					done();
+					return Response.body(body);
+				}
+			});
+
+			R.ok('hello');
+		},
+
+		'should set 200 status' (done) {
+			var r = Response.ok('');
+			var s = σ();
+			s.writeHead = (status, headers) => {
+				expect(status).to.equal(200);
+				done();
+			};
+			r.pipe(s);
+		}
+	},
+
+	'notFound': {
+		'should set body' (done) {
+			var R = Response.use({}, {
+				body(body) {
+					expect(body).to.equal('hello');
+					done();
+					return Response.body(body);
+				}
+			});
+
+			R.notFound('hello');
+		},
+
+		'should set 404 status' (done) {
+			var r = Response.notFound('');
+			var s = σ();
+			s.writeHead = (status, headers) => {
+				expect(status).to.equal(404);
+				done();
+			};
+			r.pipe(s);
+		}
+	},
+
+	'redirect': {
+		'should set empty body' (done) {
+			var R = Response.use({}, {
+				body(body) {
+					expect(body).to.be.empty();
+					done();
+					return Response.body(body);
+				}
+			});
+
+			R.redirect('/hello');
+		},
+
+		'should set location header' (done) {
+			var r = Response.redirect('/hello');
+			var s = σ();
+			s.writeHead = (status, headers) => {
+				expect(headers).to.have.property('location', '/hello');
+				done();
+			};
+			r.pipe(s);
+		},
+
+		'should set 302 status' (done) {
+			var r = Response.redirect('/hello');
+			var s = σ();
+			s.writeHead = (status, headers) => {
+				expect(status).to.equal(302);
+				done();
+			};
+			r.pipe(s);
+		},
+
+		'should set custom status' (done) {
+			var r = Response.redirect('/hello', 307);
+			var s = σ();
+			s.writeHead = (status, headers) => {
+				expect(status).to.equal(307);
+				done();
+			};
+			r.pipe(s);
+		}
 	}
 };
