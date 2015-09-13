0981ff1345a758f83008ef099cdbc3faf2c15533
Matt Brennan <mattyb1000@gmail.com>
2015-09-13 21:38:19 +0100

    feat: rename server.withServer to server.withCreateServer

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var jalfrezi = require('jalfrezi');
 var Response = require('./lib');
 
 module.exports = jalfrezi({
-	server: require('http').createServer,
+	createServer: require('http').createServer,
 	errorHandler: function(e) {
 		return Response.internalServerError(e.stack || e.toString());
 	}
 }, function server(options, handler) {
-	return options.server(function(req, res) {
+	return options.createServer(function(req, res) {
 		return handler(req)
 			.flatMapErrors(options.errorHandler)
 			.pipe(res);
